ef870c3071dae2c855e5937de9f4a60c15e733b9
ghorus <nguyen.victor4@gmail.com>
2025-02-22 15:18:52 -0800

    first commit

--- flask_template/__init__.py
+++ flask_template/__init__.py
@@ -3,12 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from flask_template.a_route.routes import a_route
-app.register_blueprint(a_route)
+from flask_template.main.routes import main
 
-@app.route('/')
-def index():
-    message = "Hi"
-    return render_template('index.html', message=message)
+app.register_blueprint(a_route)
+app.register_blueprint(main)
+
 
 if __name__ == '__main__':
     app.run()
