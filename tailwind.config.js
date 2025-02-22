a97ec74ac6e247cd5801d7fe0e41ca27f410cb5c
ghorus <nguyen.victor4@gmail.com>
2025-02-22 15:37:56 -0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./flask_template/templates/**/*.html", "./flask_template/static/js/**/*.js", "./flask_template/static/css/**/*.css"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBgColor: "#FFF9E5",
+      }
+    },
   },
   plugins: [],
 }
