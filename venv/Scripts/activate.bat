a708783b99b79826de77c22601a3dc4314d3cd00
ghorus <nguyen.victor4@gmail.com>
2025-02-22 14:35:23 -0800

    first commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\12103\Documents\softeng\web dev agency\venv
+set VIRTUAL_ENV=C:\Users\12103\Documents\softeng\flask template\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
